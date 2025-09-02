97f1e5b6513e40176be826e8061227879f49fe9f
nmihaile <148884235+nmihaile@users.noreply.github.com>
2025-09-02 11:39:40 +0200

    refactores compare_stdout() and uses a pipe to capture stdout

--- framework/src/compare_stdout.c
+++ framework/src/compare_stdout.c
@@ -6,83 +6,98 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/31 19:04:25 by nmihaile          #+#    #+#             */
-/*   Updated: 2025/08/31 21:27:34 by nmihaile         ###   ########.fr       */
+/*   Updated: 2025/09/02 11:36:28 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libunit.h"
 
-static int	redirect_stdout(const char *filename, int *saved_stdout)
+static int	redirect_stdout(int *pipe_fds, int *saved_stdout)
 {
-	int	fd;
-
 	*saved_stdout = dup(STDOUT_FILENO);
 	if (*saved_stdout == -1)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
+	if (pipe(pipe_fds) == -1)
 	{
 		close(*saved_stdout);
 		return (-1);
 	}
-	if (dup2(fd, STDOUT_FILENO) == -1)
+	if (dup2(pipe_fds[1], STDOUT_FILENO) == -1)
 	{
-		close(fd);
+		close(pipe_fds[0]);
+		close(pipe_fds[1]);
 		close(*saved_stdout);
 		return (-1);
 	}
-	return (fd);
-}
-
-static int	read_file(const char *filename, char *buff, size_t buff_size)
-{
-	int		fd;
-	ssize_t	bytes_read;
-
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (-1);
-	memset(buff, 0, buff_size);
-	bytes_read = read(fd, buff, buff_size - 1);
-	close(fd);
-	remove(filename);
-	if (bytes_read < 0)
-		return (-1);
 	return (0);
 }
 
-static int	restore_stdout(int saved_stdout, int fd)
+static int	restore_stdout(int saved_stdout, int *pipe_fds)
 {
-	int	error;
-
-	error = 0;
 	if (dup2(saved_stdout, STDOUT_FILENO) == -1)
-		error = -1;
+	{
+		close(pipe_fds[0]);
+		close(pipe_fds[1]);
+		close(saved_stdout);
+		return (-1);
+	}
 	close(saved_stdout);
-	close(fd);
-	return (error);
+	return (0);
+}
+
+// cl -> Cap Len
+static char	*capture_stdout(int fd)
+{
+	size_t	cl[2];
+	ssize_t	bytes_read;
+	char	*buf_tmp[2];
+
+	memcpy(cl, (size_t[]){1024, 0}, sizeof(cl));
+	buf_tmp[0] = malloc(cl[0]);
+	if (buf_tmp[0] == NULL)
+		return (NULL);
+	while (42)
+	{
+		if (cl[1] + 512 > cl[0])
+		{
+			cl[0] *= 2;
+			buf_tmp[1] = realloc(buf_tmp[0], cl[0]);
+			if (buf_tmp[1] == NULL)
+				return (free(buf_tmp[0]), NULL);
+			buf_tmp[0] = buf_tmp[1];
+		}
+		bytes_read = read(fd, buf_tmp[0] + cl[1], cl[0] - cl[1] - 1);
+		if (bytes_read <= 0)
+			break ;
+		cl[1] += bytes_read;
+	}
+	buf_tmp[0][cl[1]] = '\0';
+	return (buf_tmp[0]);
 }
 
 int	compare_stdout(void (*f)(void), char *expected)
 {
 	int		saved_stdout;
-	int		fd;
-	char	buff[4096];
+	int		pipe_fds[2];
+	char	*buff;
+	int		result;
 
-	fd = redirect_stdout(".cmpstdout.tmp", &saved_stdout);
-	if (fd == -1)
-	{
-		remove(".cmpstdout.tmp");
+	if (redirect_stdout(pipe_fds, &saved_stdout) == -1)
 		return (-1);
-	}
 	f();
 	fflush(stdout);
-	if (restore_stdout(saved_stdout, fd) == -1)
-	{
-		remove(".cmpstdout.tmp");
+	if (restore_stdout(saved_stdout, pipe_fds) == -1)
 		return (-1);
-	}
-	if (read_file(".cmpstdout.tmp", buff, 4096) == -1)
+	close(pipe_fds[1]);
+	buff = capture_stdout(pipe_fds[0]);
+	close(pipe_fds[0]);
+	if (buff == NULL)
 		return (-1);
-	return (strcmp(buff, expected));
+	if (expected == NULL && buff[0] == '\0')
+		return (free(buff), 0);
+	else if (expected == NULL && buff[0] != '\0')
+		return (free(buff), -1);
+	result = strcmp(buff, expected);
+	free(buff);
+	return (result);
 }
